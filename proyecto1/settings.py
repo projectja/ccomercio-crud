d53abdaa3bf074191a765eeb409a70fc06a54f10
projectja <34483622+projectja@users.noreply.github.com>
2021-09-05 11:02:23 +0200

    changed database

--- proyecto1/settings.py
+++ proyecto1/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -67,6 +67,8 @@ TEMPLATES = [
             ],
         },
     },
+
+
 ]
 
 WSGI_APPLICATION = 'proyecto1.wsgi.application'
@@ -74,17 +76,24 @@ WSGI_APPLICATION = 'proyecto1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         #'ENGINE': 'django.db.backends.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'expedientes',
+#         'USER': 'postgres',
+#         "PASSWORD": "A1tadis12",
+#         'HOST':'127.0.0.1',
+#         "DATABASE_PORT": "5432",
+#     }
+# }
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'expedientes',
-        'USER': 'postgres',
-        "PASSWORD": "A1tadis12",
-        'HOST':'127.0.0.1',
-        "DATABASE_PORT": "5432",
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
 }
 
 
